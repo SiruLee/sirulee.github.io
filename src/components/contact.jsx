bc62d59122b059522517ba52395e855688876761
Jiheon lee <jhlee520801@gmail.com>
2022-08-14 23:47:26 +0900

    Add Blinder

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,6 +1,6 @@
 import "../styles/contact.css";
 import useMeasure from "react-use-measure";
-import { motion, useDragControls } from "framer-motion";
+import { AnimatePresence, motion, useDragControls } from "framer-motion";
 import { useState } from "react";
 const min = (x, y) => {
   return x > y ? y : x;
@@ -20,8 +20,8 @@ function Contact({ dark }) {
   const [release, setRelease] = useState(false);
   const [pointer, setPointer] = useState({ x: 0, y: 0 });
   const [dragAmount, setDragAmount] = useState(0);
-  const [dotSize, setDotSize] = useState(1);
-  const [blur, setBlur] = useState(8);
+  const [dotSize, setDotSize] = useState(2);
+  const [blur, setBlur] = useState(10);
   return (
     <div className="contents" onPointerDown={startDrag}>
       <motion.div
@@ -41,42 +41,69 @@ function Contact({ dark }) {
           setDragAmount(
             Math.sqrt(Math.pow(info.offset.x, 2) + Math.pow(info.offset.y, 2))
           );
-          setBlur(8 - min(8, Math.sqrt(dragAmount) / 1.7));
-          setDotSize(min(200, Math.pow(dragAmount, 1 / 1.4)));
+          setBlur(10 - min(10, Math.sqrt(dragAmount) / 1.5));
+          setDotSize(min(15, Math.pow(dragAmount, 0.8) / 8 + 2));
         }}
         onDragEnd={() => {
           setRelease(true);
-          setBlur(8);
+          setBlur(10);
           var counter = dotSize;
           var decre;
           var x = 1;
           var interval = setInterval(() => {
             setDotSize(counter);
-            decre = Math.pow(dotSize, 1.03) / 4 / Math.pow(x, 1.1);
+            decre = Math.pow(dotSize, 1.01) / 7 / Math.pow(x, 1 / 1.2);
             counter -= decre;
             x++;
-            if (counter <= 1) {
-              setDotSize(1);
+            if (counter <= 2) {
+              setDotSize(2);
+              setRelease(false);
               clearInterval(interval);
             }
           }, 10);
         }}
         id="draggable"
       ></motion.div>
-      <div
-        id="blinder"
-        style={{
-          transition: release ? "1s ease" : "",
-          backdropFilter: `blur(${blur}px)`,
-          WebkitBackdropFilter: `blur(${blur}px)`,
-          backgroundImage: dark
-            ? `radial-gradient(rgba(0, 0, 0, 0) 2px, #292929 ${dotSize}px)`
-            : `radial-gradient(rgba(0, 0, 0, 0) 2px, white ${dotSize}px)`,
-        }}
-      ></div>
       <div id="contact" ref={ref}>
+        <AnimatePresence exitBeforeEnter>
+          {dark && (
+            <motion.div
+              key="dark-blinder"
+              id="dark-blinder"
+              className="blinder"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.2, delay: 0.1 }}
+              style={{
+                transition: release ? "1s ease" : "",
+                backdropFilter: `blur(${blur}px)`,
+                WebkitBackdropFilter: `blur(${blur}px)`,
+                backgroundImage: `radial-gradient(rgba(0, 0, 0, 0) ${dotSize}px, #292929 1px)`,
+                zIndex: dark ? 4 : 2,
+              }}
+            ></motion.div>
+          )}
+          {!dark && (
+            <motion.div
+              key="light-blinder"
+              id="light-blinder"
+              className="blinder"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.2, delay: 0.1 }}
+              style={{
+                transition: release ? "1s ease" : "",
+                backdropFilter: `blur(${blur}px)`,
+                WebkitBackdropFilter: `blur(${blur}px)`,
+                backgroundImage: `radial-gradient(rgba(0, 0, 0, 0) ${dotSize}px, white 1px)`,
+                zIndex: dark ? 2 : 4,
+              }}
+            ></motion.div>
+          )}
+        </AnimatePresence>
         <div
           id="gh"
+          className="contactContent"
           style={{
             fontSize: min(
               contactInfo["height"] * 0.2,
@@ -89,6 +116,7 @@ function Contact({ dark }) {
 
         <div
           id="email"
+          className="contactContent"
           style={{
             fontSize: min(
               contactInfo["height"] * 0.2,
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import App from "./App";
 import "./index.css";
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty("--vh", `${vh}px`);
+let vw = window.innerWidth * 0.01;
+document.documentElement.style.setProperty("--vw", `${vw}px`);
 window.addEventListener("resize", () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
+  let vw = window.innerWidth * 0.01;
+  document.documentElement.style.setProperty("--vw", `${vw}px`);
 });
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
